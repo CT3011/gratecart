ade1e8328ebe680d2254b1024adc8f992f2f4987
Chirag <chiragThengadi123@gmail.com>
2023-07-12 16:38:01 +0530

    Allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-greatkart-env.eba-qwn2yji5.us-west-2.elasticbeanstalk.com', "*"]
+ALLOWED_HOSTS = ['django-env.eba-3mh3wgsn.us-west-2.elasticbeanstalk.com', "*"]
 
 
 # Application definition
